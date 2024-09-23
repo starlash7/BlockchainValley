e9ee45407194a44049d7586a0641de4359cc680b
starlash7 <yang92sw@gmail.com>
2024-09-23 16:15:52 +0900

    함수 값

--- Frontend/JavaScript/assets/scripts/app.js
+++ Frontend/JavaScript/assets/scripts/app.js
@@ -114,5 +114,25 @@
 //   console.log(hobby);
 // }
 
-const list = document.querySelector("ul");
-list.remove();
+// const list = document.querySelector("ul");
+// list.remove();
+
+function handleTimeout() {
+  console.log("Timed out!");
+}
+
+const handleTimeout2 = () => {
+  console.log("Timed out! ..agine!");
+};
+
+setTimeout(handleTimeout, 2000);
+setTimeout(handleTimeout2, 3000);
+setTimeout(() => {
+  console.log(`More timing out...`);
+}, 4000);
+
+function greeter(greetFn) {
+  greetFn();
+}
+
+greeter(() => console.log("Hi"));

7fe6d7d25c2c930967c9aef38b9dd997dd560cea
starlash7 <yang92sw@gmail.com>
2024-09-23 14:37:16 +0900

    변수

--- Frontend/JavaScript/assets/scripts/app.js
+++ Frontend/JavaScript/assets/scripts/app.js
@@ -1,6 +1,11 @@
-import { apiKey, abc } from "./util.js";
-//import apiKey from "./util.js";
-import * as util from "./util.js";
-// util.js 에서 제공하는 모든것을 한번에 그롭화해서 import
+// import { apiKey, abc } from "./util.js";
+// //import apiKey from "./util.js";
+// import * as util from "./util.js";
+// // util.js 에서 제공하는 모든것을 한번에 그롭화해서 import
 
-console.log(apiKey);
+// console.log(apiKey);
+
+const userMessage = "Hello World!";
+
+console.log(userMessage);
+console.log(userMessage);

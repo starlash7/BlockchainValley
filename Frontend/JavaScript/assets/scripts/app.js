ea799394e4a888d7dc99f23d9cb893d2ce7765e3
starlash7 <yang92sw@gmail.com>
2024-09-23 16:18:16 +0900

    함수 내부

--- Frontend/JavaScript/assets/scripts/app.js
+++ Frontend/JavaScript/assets/scripts/app.js
@@ -117,22 +117,31 @@
 // const list = document.querySelector("ul");
 // list.remove();
 
-function handleTimeout() {
-  console.log("Timed out!");
+// function handleTimeout() {
+//   console.log("Timed out!");
+// }
+
+// const handleTimeout2 = () => {
+//   console.log("Timed out! ..agine!");
+// };
+
+// setTimeout(handleTimeout, 2000);
+// setTimeout(handleTimeout2, 3000);
+// setTimeout(() => {
+//   console.log(`More timing out...`);
+// }, 4000);
+
+// function greeter(greetFn) {
+//   greetFn();
+// }
+
+// greeter(() => console.log("Hi"));
+
+function init() {
+  function greet() {
+    console(`HI!`);
+  }
+  greet();
 }
 
-const handleTimeout2 = () => {
-  console.log("Timed out! ..agine!");
-};
-
-setTimeout(handleTimeout, 2000);
-setTimeout(handleTimeout2, 3000);
-setTimeout(() => {
-  console.log(`More timing out...`);
-}, 4000);
-
-function greeter(greetFn) {
-  greetFn();
-}
-
-greeter(() => console.log("Hi"));
+init();

800dda96d8f50693e0ef0b34b5bed6ef47d0ec6b
starlash7 <yang92sw@gmail.com>
2024-09-25 11:59:24 +0900

    React 기초끝

--- Frontend/React/01-starting-project/src/App.jsx
+++ Frontend/React/01-starting-project/src/App.jsx
@@ -38,10 +38,9 @@ function App() {
         <seciton id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
           </ul>
         </seciton>
         <section id="examples">
